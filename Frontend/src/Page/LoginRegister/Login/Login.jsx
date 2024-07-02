5684ae0a06071599520cbf9912c78b36b71fe101
adhaghani <adhaahmad04@gmail.com>
2024-07-02 16:12:56 +0800

    Updated Login Page (Added Button, Input), Adjusted Input CSS

--- Frontend/src/Page/LoginRegister/Login/Login.jsx
+++ Frontend/src/Page/LoginRegister/Login/Login.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 import "../LoginRegister.css";
 import FormTitle from "../Component/FormTitle";
+import Input from "../../../Component/Input/Input";
+import Button from "../../../Component/Button/Button";
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div className="LoginRegistesr" id="LoginRegister">
@@ -13,7 +16,44 @@ const Login = () => {
         </div>
         <div className="Form-Container">
           <FormTitle Title="Login" Details="Enter your credentials to login." />
-          <form action=""></form>
+          <form action="">
+            <div className="Input-Container">
+              <Input
+                formSize="full"
+                inputProps={{
+                  type: "text",
+                  name: "Username",
+                  id: "Username",
+                  label: "Username",
+                  placeholder: "Username",
+                  className: "input"
+                  // value: Input.Username,
+                  // onChange: handleChange
+                }}
+              />
+              <Input
+                formSize="full"
+                inputProps={{
+                  type: "password",
+                  name: "Password",
+                  id: "Password",
+                  label: "Password",
+                  placeholder: "password",
+                  className: "input"
+                  // value: Input.Password,
+                  // onChange: handleChange
+                }}
+              />
+              <div className="Link-Container">
+                <p>
+                  New User? <Link to={"/Register"}>Register now</Link>
+                </p>
+              </div>
+            </div>
+            <div className="Button-Contianer">
+              <Button value="Login" className="Full Fill" link="/Dashboard" />
+            </div>
+          </form>
         </div>
       </div>
     </div>
